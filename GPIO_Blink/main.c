9684a34f465030a3283620caa99efe7841131ec9
Rinki Rajak <razakrinky@gmail.com>
2026-03-10 22:31:47 +0530

    Update README

---
+++ GPIO_Blink/main.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+
+// RCC register
+#define RCC_APB2ENR (*(volatile uint32_t*)0x40021018)
+
+// GPIOC registers
+#define GPIOC_CRH   (*(volatile uint32_t*)0x40011004)
+#define GPIOC_ODR   (*(volatile uint32_t*)0x4001100C)
+
+void delay()
+{
+    for(volatile int i = 0; i < 500000; i++);
+}
+
+int main()
+{
+    // Enable GPIOC clock
+    RCC_APB2ENR |= (1 << 4);
+
+    // Configure PC13 as output push-pull
+    GPIOC_CRH &= ~(0xF << 20);
+    GPIOC_CRH |=  (0x1 << 20);
+
+    while(1)
+    {
+        GPIOC_ODR ^= (1 << 13); // Toggle LED
+        delay();
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Embedded Projects
+
+This repository contains STM32 bare-metal embedded projects.
+
+## Projects
+
+### GPIO Blink
+Bare-metal LED blinking using direct register access on STM32F103.
+
+More projects coming soon:
+- Timer PWM
+- UART Communication
+- FreeRTOS
